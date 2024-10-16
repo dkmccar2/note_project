7152c094a7d54769d47b47252e4bf39c6a89a887
dkmccar2 <127516244+dkmccar2@users.noreply.github.com>
2024-10-16 11:53:03 -0400

    Commit client and server
    Commit client and server

---
+++ client/src/components/App.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/getnotes")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log("Notes loaded from database: " + data);
+      });
+  }, []);
+
+  const handleAddItem = async (newNote) => {
+    console.log("Adding note.. " + newNote);
+    let id;
+    try {
+      await axios
+        .post("http://localhost:4000/addnote", {
+          title: newNote.title,
+          content: newNote.content,
+        })
+        .then((res) => {
+          console.log(res);
+          // console.log(res.data);
+          id = res.data;
+        });
+    } catch (error) {
+      console.error("Error adding item:", error);
+    }
+
+    if (id) {
+      console.log(id);
+      setNotes((prevNotes) => {
+        return [
+          ...prevNotes,
+          { id: id, title: newNote.title, content: newNote.content },
+        ];
+      });
+    }
+  };
+
+  const handleDeleteItem = (id) => {
+    console.log("Deleting note with id: " + id);
+
+    try {
+      axios.delete(`http://localhost:4000/deletenote/${id}`).then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+    } catch (error) {}
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return noteItem.id !== id;
+      });
+    });
+  };
+  // function addNote(newNote) {
+  //   // console.log(newNote);
+  //   try {
+  //     const response = async () => {
+  //       await fetch("http://localhost:4000/addnote", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(newNote),
+  //       });
+  //     };
+  //     //console.log(response);
+  //   } catch (error) {
+  //     console.error("Error adding item:", error);
+  //   }
+  //   setNotes((prevNotes) => {
+  //     return [...prevNotes, newNote];
+  //   });
+  // }
+
+  // function deleteNote(id) {
+  //   setNotes((prevNotes) => {
+  //     return prevNotes.filter((noteItem, index) => {
+  //       return index !== id;
+  //     });
+  //   });
+  // }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={handleAddItem} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={noteItem.id}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={handleDeleteItem}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+const { Pool } = pg;
+const pool = new Pool({
+  //env variables
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+const app = express();
+const port = 4000;
+
+app.use(cors(corsOptions)); //middleware
+app.use(express.json());
+
+app.get("/getnotes", async (req, res) => {
+  console.log("<------------------------>");
+  console.log("Get route activated");
+
+  const result = await pool.query("SELECT * FROM notes");
+  if (result) {
+    console.log("Existing notes loaded from db");
+  }
+  res.send(result.rows);
+});
+
+app.post("/addnote", async (req, res) => {
+  console.log("<------------------------>");
+  console.log("Post route activated");
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const result = await pool.query(
+    `INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING id`,
+    [title, content]
+  );
+  const idReturn = result.rows[0].id;
+  console.log("Returning id " + result.rows[0].id);
+
+  if (result) {
+    console.log("Note added to database");
+  }
+  res.json(idReturn);
+});
+
+app.delete("/deletenote/:id", async (req, res) => {
+  console.log("<------------------------>");
+  console.log("Delete route activated");
+  const id = parseInt(req.params.id);
+  const data = req.body;
+
+  console.log("Deleting note with id: " + id);
+
+  const result = await pool.query(`DELETE FROM notes WHERE id = ${id}`);
+
+  if (result) {
+    console.log("Note successfully deleted");
+  }
+  res.json({ message: "Data received successfully", data });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}, http://localhost:${port}`);
+});
